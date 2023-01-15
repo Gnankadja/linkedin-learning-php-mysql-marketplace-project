5b76d3d574b28bbadaf508f507ef3a0863007c0f
Nathan Gnankadja <gnankadjanathan@gmail.Com>
2023-01-15 22:42:06 +0100

    Resovle jss css file link

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,5 @@
-</body>
 
+<script type="application/javascript" src="assets/js/jquery-3.2.1.slim.min.js"></script>
+<script type="application/javascript" src="assets/js/bootstrap.min.js"></script>
+</body>
 </html>
